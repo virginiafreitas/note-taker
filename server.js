5f70e916c6f6d402acfb907a6d82c44b373741c6
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-31 20:49:16 -0700

    import uuid

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const fs = require('fs'); // Imports Node.js file system module for file operati
 const app = express(); // Creates an instance of the Express application
 const PORT = 3000; // Defines the port number to listen to
 
+const { v4: uuidv4 } = require('uuid'); // Imports UUID (Universally Unique Identifier) module to generate unique IDs
+
