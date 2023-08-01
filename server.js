9c58e617fb6f6e73d4c09685b3ce46aaf96970a5
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-31 20:47:13 -0700

    imported modules

---
+++ server.js
@@ -0,0 +1,4 @@
+// imported required modules
+const express = require('express'); // Imports the Express.js framework
+const path = require('path'); // Imports Node.js path module for working with file paths
+const fs = require('fs'); // Imports Node.js file system module for file operations
