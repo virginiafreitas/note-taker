0772e3e83fbe571a26333a85ad624827c1b8a379
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-31 20:48:40 -0700

    create express instance and port

--- server.js
+++ server.js
@@ -2,3 +2,7 @@
 const express = require('express'); // Imports the Express.js framework
 const path = require('path'); // Imports Node.js path module for working with file paths
 const fs = require('fs'); // Imports Node.js file system module for file operations
+
+const app = express(); // Creates an instance of the Express application
+const PORT = 3000; // Defines the port number to listen to
+
